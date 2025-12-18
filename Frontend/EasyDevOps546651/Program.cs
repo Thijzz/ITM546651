e65cbcec6664f3482d243918e84591a88846ced5
Thijs <thijsrewinkel@gmail.com>
2025-12-18 12:47:46 +0100

    Frontend gedaan

---
+++ Frontend/EasyDevOps546651/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        // Print tekst
+        Console.WriteLine("EasyDevOps");
+
+        // Open logo.png vanuit Downloads
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) 
+                      + "/Downloads/logo.png";
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = path,
+            UseShellExecute = true
+        });
+    }
+}
